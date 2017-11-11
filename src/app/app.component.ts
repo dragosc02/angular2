7ca70e38593e2f9b2edb98bce5ddf02ada3e66ef
Dragos Cretu <dragos.cretu@gmail.com>
2017-11-11 18:24:08 +0200

    Initial commit
    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
+
+@Component({
+  selector: 'pm-root',
+  template: `<div>
+    <h1>{{pageTitle}}</h1>  
+    <pm-products></pm-products>
+</div>`,  
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
